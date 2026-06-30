6ebfe7d61487eb405b17a40d7f064f92d1763803
yahyaoncloud <yahyakhan.islam2@gmail.com>
2026-06-30 19:30:23 +0530

    feat: migrate to React Router with Vercel configuration and dependencies

---
+++ react-router.config.ts
@@ -0,0 +1,8 @@
+import { vercelPreset } from '@vercel/react-router/vite';
+import type { Config } from '@react-router/dev/config';
+
+export default {
+  // Server-side render by default, to enable SPA mode set this to `false`
+  ssr: true,
+  presets: [vercelPreset()],
+} satisfies Config;
